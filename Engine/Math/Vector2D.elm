b640db604b51039488c29c0f52784fafccd52b6f
NysoS <nwanysos79@gmail.com>
2022-12-14 22:59:05 +0100

    refacto vector2d

--- Engine/Math/Vector2D.elm
+++ Engine/Math/Vector2D.elm
@@ -7,16 +7,4 @@ type alias Vector2D =
 
 createVector2D : ( Float, Float) -> Vector2D
 createVector2D (x, y) =
-    let
-        tempX =
-            if x != 0 then
-                x
-            else
-                0
-        tempY =
-            if y != 0 then
-                y
-            else
-                0
-    in
-    (Vector2D tempX tempY)
+    (Vector2D x y)
